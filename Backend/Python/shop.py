c9749448a32e42b237b339cba77551be758c1704
bfwi124abo <christian.boll@edu.fhdw.de>
2025-10-02 10:37:05 +0200

    implemented function /category_bar in shop.py

--- Backend/Python/shop.py
+++ Backend/Python/shop.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,10 +8,12 @@ def index():
 
 @app.route("/cart")
 def cart():
+    # read category - Request: I use Request to read the category from the URL
+    category = request.args.get("category") # looks for the parameter "categorys"
     return render_template("cart.html")
 
-@app.route("/produkte")
-def produkte():
+@app.route("/products")
+def products():
     produkte = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
     html = """
 <title>Slideshow Galerie</title>
@@ -58,6 +60,29 @@ function showSlides(n, no) {
     """
     
     return html  # Returns raw HTML
+
+# Function to generate the category bar at the top of the page
+@app.route("/category_bar")
+def category_bar():
+    # temporary - future: will be filled dynamically out of the DB
+    categorys = ["category A", "category B", "category C", "category D", "category E", "category F", "category G", "category H", "category I", "category J", "category K", "category L"]
+    # generating html-code
+    html = ""
+    for c in categorys:
+        link = f"/cart?category={c}" # creating a link to reach the page for the category-overview
+        html += f"""
+          <div class="entry"><a href="{link}">{c}</div> 
+        """
+    # returning generated code
+    return html
+
+@app.route("/bottomBar")
+def bottom_bar():
+    return """
+    <div class="bottom_bar">
+        <p>© 2025 Was-wird-shop | Impressum | Datenschutz</p>
+    </div>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
